0409afaeb2817b35097c371d74ea3a4038a1ca0b
MariamMamdouh <mariam.melshami@gmail.com>
2018-02-15 20:26:56 +0200

    Edits in Helping community page GUI Edits in New_Campgain_Equity GUI

--- app/src/main/java/com/example/shaza/graduationproject/Activities/SupportStartUp.java
+++ app/src/main/java/com/example/shaza/graduationproject/Activities/SupportStartUp.java
@@ -35,7 +35,8 @@ public class SupportStartUp extends AppCompatActivity {
         Intent expertChat = new Intent(this, TalkToExpert.class);
         startActivity(expertChat);
     }
-    public void  goToEquityCampaign(View view){
+
+    public void goToEquityCapmpagin(View view) {
         Intent equityPage = new Intent(this,Equity_campaign_Home_page.class);
         startActivity(equityPage);
     }
